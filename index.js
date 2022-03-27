ef9f08bee28d7e253775a146e918e42d664bd292
Trafael Lewis <TrafLewis@gmail.com>
2022-03-27 15:50:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function praise(adj = "special") {
+        return `You are ${visualFlair}${adj}${visualFlair}!`;
+
+    }
+    
+}
